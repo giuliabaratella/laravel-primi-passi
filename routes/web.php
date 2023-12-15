4710a3eeabff6b50291d8ac95b32b6ec32eeb0f4
giuliabaratella <giuliabaratella7@gmail.com>
2023-12-15 15:30:58 +0100

    added file home + printed Hello World

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'text' => 'Hello World!',
+    ];
+    return view('home', $data);
 });
